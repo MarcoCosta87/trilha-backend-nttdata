4b33ba3a806352cc847755d6ad02f80358eb0fee
MarcoCosta87 <marcoacj018@gmail.com>
2022-01-20 22:35:44 -0300

    desafio6

---
+++ src/main/java/br/com/controlefinanceiro/dto/EntryDto.java
@@ -0,0 +1,69 @@
+package br.com.controlefinanceiro.dto;
+
+public class EntryDto {
+
+	private String name;
+
+	private String description;
+
+	private String type;
+
+	private String amount;
+
+	private String date;
+
+	private boolean paid;
+
+	public EntryDto() {
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getAmount() {
+		return amount;
+	}
+
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
+	}
+
+	public boolean isPaid() {
+		return paid;
+	}
+
+	public void setPaid(boolean paid) {
+		this.paid = paid;
+	}
+
+	
+}
